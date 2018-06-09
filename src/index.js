57d356a14fe1f80ab032901e79ae01995551ef07
Kris <kris.howard@gmail.com>
2018-06-09 18:55:19 +1000

    Configuring event values

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 var request = require('request');
 var secrets = require('./secrets.json');
+var icalToolkit = require('ical-toolkit');
+
+//Create a builder
+var builder = icalToolkit.createIcsFileBuilder();
+
+/*
+ * Settings (All Default values shown below. It is optional to specify)
+ * */
+builder.spacers = true; //Add space in ICS file, better human reading. Default: true
+builder.NEWLINE_CHAR = '\r\n'; //Newline char to use.
+builder.throwError = false; //If true throws errors, else returns error when you do .toString() to generate the file contents.
+builder.ignoreTZIDMismatch = true; //If TZID is invalid, ignore or not to ignore!
 
 function handler(event, context) {
     var options = {
@@ -20,7 +32,59 @@ function handler(event, context) {
             return context.fail({error: body});
         }
         try {
-            var result = JSON.parse(body);
+
+            /**
+             * Build ICS
+             * */
+
+            //Name of calander 'X-WR-CALNAME' tag.
+            builder.calname = 'Test Meetup Calendar';
+
+            //Cal timezone 'X-WR-TIMEZONE' tag. Optional. We recommend it to be same as tzid.
+            builder.timezone = 'australia/sydney';
+
+            //Time Zone ID. This will automatically add VTIMEZONE info.
+            builder.tzid = 'australia/sydney';
+
+            //Method
+            builder.method = 'REQUEST';
+
+            var meetupevents = JSON.parse(body);
+            var e = meetupevents.results[0];
+
+            //Add events
+            builder.events.push({
+              //Event start time, Required: type Date()
+              start: new Date(e.time),
+
+              //Event end time, Required: type Date()
+              end: new Date(),
+
+              //Event summary, Required: type String
+              summary: e.group.name,
+
+              //All Optionals Below
+
+              //Event identifier, Optional, default auto generated
+              uid: 'event_' + e.id + '@meetup.com',
+
+              //Optional, The sequence number in update, Default: 0
+              sequence: null,
+
+              //Location of event, optional.
+              location: e.location,
+
+              //Optional description of event.
+              description: e.description,
+
+              //Status of event
+              status: 'CONFIRMED',
+
+              //Url for event on core application, Optional.
+              url: e.event_url
+            });
+
+            var result = icalToolkit.parseToJSON(builder.toString());
         }
         catch (e) {
             return context.fail({error: 'Could not parse body: ' + body});
