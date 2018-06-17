956992c44cb00e15b7319296acc0389c29f55ca8
Kris <kris.howard@gmail.com>
2018-06-17 22:35:26 +1000

    Using async.each to parallelise SQS message sends

--- src/getgroups/index.js
+++ src/getgroups/index.js
@@ -3,6 +3,7 @@ var secrets = require('./secrets.json');
 var AWS = require('aws-sdk');
 var QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/613444755180/meetupgroups';
 var sqs = new AWS.SQS({region : 'us-east-1'});
+var async = require('async');
 
 function handler(event, context, callback) {
 
@@ -32,25 +33,32 @@ function handler(event, context, callback) {
     }
     try {
       var meetupgroups = JSON.parse(body);
-      for (var i = 0, len = meetupgroups.length; i < len; i++) {
-        console.log('group: ' + JSON.stringify(meetupgroups[i].urlname));
-        var params = {
-          MessageBody: JSON.stringify(meetupgroups[i].urlname),
-          QueueUrl: QUEUE_URL
-        };
-        sqs.sendMessage(params, function(err,data){
-          if(err) {
-            console.log('error:',"Fail Send Message" + err);
-            return;
-          }
-          console.log('data:',data.MessageId);
-        });
-      }
     }
     catch (e) {
       return context.fail('Could not parse groups body: ' + body);
     }
-    return context.succeed('Success!');
+    async.each(meetupgroups, function(group, groupdone){
+      console.log('group: ' + JSON.stringify(group.urlname));
+      var params = {
+        MessageBody: JSON.stringify(group.urlname),
+        QueueUrl: QUEUE_URL
+      };
+      sqs.sendMessage(params, function(err,data){
+        if(err) {
+          console.log('error:',"Fail Send Message" + err);
+        } else {
+          console.log('data:',data.MessageId);
+        }
+        groupdone(err);
+      });
+    }, function(err){
+      if(err) {
+        console.log('One of the messages failed to send');
+        return context.fail(err);
+      } else {
+        return context.succeed('Success!');
+      }
+    });
   });
 }
 
