d52e7a6797617272a9e5d040a862236654d88d2f
Kris <kris.howard@gmail.com>
2019-07-27 14:12:07 +1000

    Changed to use OAuth access token instead of API key

--- src/getgroups/index.js
+++ src/getgroups/index.js
@@ -12,7 +12,7 @@ exports.handler = async function(event, context, callback) {
       qs: {
         'country': 'AU',
         'upcoming_events': 'true',
-        'key': secrets.meetup_api_key,
+        'access_token': secrets.access_token,
         'location': targetlocation + ', Australia',
         'topic_id': '48471,17628,15582,3833,84681,79740,21549,21441,18062,15167,10209,124668,116249,7029',
         //'topic_id': '79740' // testing
